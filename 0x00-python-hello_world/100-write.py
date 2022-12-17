f155ce596ad1003791d1d49f3a264e3df7707198
daposarumi <sarumiadedapo@gmail.com>
2022-12-17 14:23:53 +0000

    Edit

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from sys import stderr
 
-stderr.write("and that pice of art is useful - Dora Korpar, 2015-10-19\n")
+stderr.write("and that pice of art is useful - Dora Korpar, 2015-10-19")
 exit(1)
