6023114ce4d5c2fa21cae1d50e39735ff21e00d9
daposarumi <sarumiadedapo@gmail.com>
2023-02-17 11:55:25 +0000

    Fix order

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,6 +4,6 @@ def print_sorted_dictionary(a_dictionary):
     by ordered keys
     """
     dict_keys = list(a_dictionary)
-    sorted(a_dictionary)
+    dict_keys = sorted(a_dictionary)
     for i in dict_keys:
         print("{}: {}".format(i, a_dictionary.get(i)))
