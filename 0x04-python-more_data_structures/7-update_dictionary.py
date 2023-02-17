4744c1dace7325a77faefbd44994a49876e6cf5d
daposarumi <sarumiadedapo@gmail.com>
2023-02-17 12:09:01 +0000

    code updates dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """function replaces or adds
+    keys/values in dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
