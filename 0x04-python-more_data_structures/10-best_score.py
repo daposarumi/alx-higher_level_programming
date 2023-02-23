e201901060336a9abc2a8b1abc307a871b3ef201
daposarumi <sarumiadedapo@gmail.com>
2023-02-23 12:14:25 +0000

    Add function that returns highest value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin.python3
+def best_score(a_dictionary):
+    """function returns key
+    with the biggest integer value
+    """
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
