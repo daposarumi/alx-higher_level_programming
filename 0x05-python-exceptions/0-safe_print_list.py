684e81ed28ec2a0916736ac38c47ab982492522d
daposarumi <sarumiadedapo@gmail.com>
2023-02-24 11:57:17 +0000

    Add new folder

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """This function prints x elements of a
+    list
+    """
+
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This folder is for functions on exceptions in python
